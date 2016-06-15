a36c7c41a637c55c1e9bb291b66c7ce23a604449
Fenrir <fenrirwolf@gmail.com>
2016-06-14 22:53:52 -0700

    Add error module

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,14 @@
 #![feature(filling_drop)]
 #![feature(float_extras)]
 #![feature(heap_api)]
+#![feature(int_error_internals)]
 #![feature(lang_items)]
 #![feature(macro_reexport)]
 #![feature(oom)]
 #![feature(prelude_import)]
 #![feature(rand)]
 #![feature(raw)]
+#![feature(reflect_marker)]
 #![feature(slice_concat_ext)]
 #![feature(start)]
 #![feature(stmt_expr_attributes)]
@@ -58,6 +60,8 @@ pub use core::ptr;
 pub use core::raw;
 pub use core::result;
 pub use core::option;
+
+pub mod error;
 
 pub use alloc::arc;
 pub use alloc::boxed;
